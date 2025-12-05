c2bdbb8e261f45dfb1c5c4476058b89162c927d2
konraku <albizia.tk@gmail.com>
2025-12-05 23:33:39 +0900

    test file delete

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function(){
-    return view('test');
-});
-*/
-
 // Vue.jsアプリ用のルート（すべてのパスをVue.jsに任せる）
 Route::get('/{any}', function () {
     return view('app');
