d87876243160caa8eabf02c03ca6db451a6ce750
konraku <albizia.tk@gmail.com>
2025-12-11 13:19:54 +0900

    home修正

--- resources/js/app.vue
+++ resources/js/app.vue
@@ -1,8 +1,22 @@
 <template>
-  <div class="container">
-    <h1>Hello Latch!</h1>
-    <p>スクレイピングアプリへようこそ</p>
-  </div>
+  <main>
+    <div class="container">
+      <h1 class="titlecColor" >Latch</h1>
+      <p class="textColor" >スクレイピングアプリへようこそ</p>
+
+      <div>
+        <input id="urlSearchInput" type="text" placeholder="Search or scan a URL"></input>
+      </div>
+      
+      <div>
+        <input id="searchURL" type="text"></input>
+      </div>
+    </div>
+
+    <div id="bodyContents">
+
+    </div>
+  </main>
 </template>
 
 <script>
@@ -12,6 +26,15 @@ export default {
 </script>
 
 <style scoped>
+.titlecColor {
+    color: #76aaf8;
+}
+
+.textColor {
+    color: #EAEAEA;
+    font-size: 14px;
+}
+
 .container {
   max-width: 800px;
   margin: 50px auto;
@@ -19,14 +42,19 @@ export default {
   text-align: center;
   font-family: Arial, sans-serif;
 }
-
-h1 {
+h1{
+  margin: 0px auto;
   color: #4CAF50;
-  font-size: 3rem;
+  font-size: 5rem;
 }
 
 p {
+  margin:0px auto;
   font-size: 1.2rem;
   color: #555;
+}
+
+#urlSearchInput {
+  margin-top:25px;
 }
 </style>
---
+++ resources/js/header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <div class="wrapper">
+            <div id="hamburguer">
+                
+            </div>
+        </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
