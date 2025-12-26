d5e8f011d2aa9d1030240e131ac9cc8c25d384b8
konraku <albizia.tk@gmail.com>
2025-12-26 17:21:50 +0900

    title取得

--- app/Http/Controllers/Api/ScraperController.php
+++ app/Http/Controllers/Api/ScraperController.php
@@ -4,13 +4,38 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ScraperController extends Controller
 {
     public function scrape(Request $request){
         $url = $request->input('url');
+        
+        try {
+            $response = Http::timeout(10)->get($url);
+
+            if (!$response->successful()) {
+                responseError("Failed to fetch URL", $url);
+            }
+
+            $html = $response->body();
+            preg_match('/<title>(.*?)<\/title>/i', $html, $titleMatch);
+            $title = $titleMatch[1] ?? null;
+
+            return response()->json([
+                'url' => $url,
+                'title' => $title,
+            ]);
+
+        }
+        catch(\Exception $e) {
+            responseError($e, $url);
+        }
+    }
+
+    public function responseError(string $er, string $url){
         return response()->json([
-            'message' => 'URLを受け取りました',
+            'message' => $er,
             'url' => $url
         ]);
     }
--- resources/vue/main.vue
+++ resources/vue/main.vue
@@ -64,6 +64,7 @@ export default {
       }
     };
 
+    //htmlに返している
     return {
       url,
       loading,
